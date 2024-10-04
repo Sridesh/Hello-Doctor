554d815ccc91fa28e1dcd2e47ea54d4b15e66b7b
Sridesh Fernando <srideshhimalka@gmail.com>
2024-10-04 22:40:17 +0530

    Added auth screens

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hello-Doctor
-A chat app built using ReactJS, NodeJS, ExpressJS &amp; MongoDB and Stream
--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
 import "./App.css";
 
@@ -10,7 +10,11 @@ const apiKey = "n9nk5b6ys2ra";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 function App() {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
